6a823f214f338c24d6075e0eb571f596a3ba9adc
CoderDan1983 <seekingsomething121@gmail.com>
2022-02-25 09:27:11 -0800

    handlebars integrated

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ require('dotenv').config();
 app.use(express.static(path.join(__dirname, './')));
 app.use(express.urlencoded({ extended: true }));
 
+//adding HBS :D
+const { engine } = require('express-handlebars');
+
+app.engine('hbs', engine({
+    layoutDir: `${__dirname}/views/layouts`,
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialDir: `${__dirname}/views/partials`,
+    helpers: { //helper available globally (ie-this for this instance)
+
+    }
+}));
+app.set('view engine', 'hbs');
+
+//"routes" :D
+app.get('/', (req, res)=>{
+    res.render('main', { layout: 'index' })
+})
+
+
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT} `);
 })
